6040c4139e6bc94971f1e9d0a5e51aa8fcd485e3
David Flores Silva <dfloressi@unsa.edu.pe>
2021-05-31 16:03:05 -0500

    se crea la clase Persona que lleva un parametro importante que es models.Model

--- personas/models.py
+++ personas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import IntegerField
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = IntegerField()
+    
