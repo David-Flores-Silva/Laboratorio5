3baad1989ee3f5a882550eabe2b0e4755891b008
David Flores Silva <dfloressi@unsa.edu.pe>
2021-05-31 14:56:04 -0500

    7.1 editamos la clase Humano que esta dentro de la app porque al realizar el paso 8 nos marcaba error

--- humanos/models.py
+++ humanos/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Humano(models.Model):
-    nombre = models.CharField()
-    sexo = models.CharField()
+    nombre = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=100)
