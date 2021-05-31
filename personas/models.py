191c5d48a17a795d5d546f880bd9ddf25a3ab92b
David Flores Silva <dfloressi@unsa.edu.pe>
2021-05-31 16:12:56 -0500

    se agrego el nuevo parametro donador que es boolean

--- personas/models.py
+++ personas/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.db.models.fields import IntegerField
+from django.db.models.fields import BooleanField, IntegerField
 
 # Create your models here.
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
     edad = IntegerField()
-    
+    donador = BooleanField()
