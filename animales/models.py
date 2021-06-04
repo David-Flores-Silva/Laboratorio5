30e62894daf0a3e48e116606ebd64af68b28fc50
David Flores Silva <dfloressi@unsa.edu.pe>
2021-06-04 10:38:18 -0500

    agrege dos variables mas a la clase animal

--- animales/models.py
+++ animales/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Animal(models.models):
     nombre = models.CharField(max_length=100)
-    numuero_patas = models.IntegerField()
-    
+    numero_patas = models.IntegerField()
+
+    carnivoro = models.BooleanField()
+    herbivoro = models.BooleanField()
